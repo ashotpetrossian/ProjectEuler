d7b65293cff25db16f2c0f63add9c7a809b412e9
Ashot Petrossian <76805846+ashotpetrossian@users.noreply.github.com>
2022-03-09 20:44:11 +0400

    Update letticePaths.cpp

--- letticePaths.cpp
+++ letticePaths.cpp
@@ -1,1 +1,6 @@
-//COMBINATORICS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+int foo (int n, int m) {
+    if (m == 0 || n == 0) {
+        return 1;
+    }
+    return foo(m - 1, n) + foo(m, n - 1);
+}
